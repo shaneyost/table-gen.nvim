29b174e6b7a49825656fe5d63dbab69ebdd1124f
Shane Yost <scy@shaneyost.io>
2025-04-13 00:08:22 -0600

    Updata example with file io operations, proof of concept

--- sandbox/ingest.lua
+++ sandbox/ingest.lua
@@ -4,4 +4,8 @@ local data = require("data")
 
 local Model, vals = struct.create_new_struct(data)
 local m = Model(vals)
+
+local file = assert(io.open("table.bin", "wb"))
+file:write(m:to_bytes())
+file:close()
 print(m)
--- sandbox/struct.lua
+++ sandbox/struct.lua
@@ -16,7 +16,13 @@ typedef union
 --- Creates a formatted string that reflects a union of the passed in C struct
 --- and dynamicallyl adds a raw byte array to facilitate easier i/o operations
 ---@param data table Describes all parts of a C structure including init vals
----@return string string table
+---@return string body The string containing the body of the C structure
+---@return string sdef The union type specifier
+---@return table vals The table of values for initialization
+---
+---@usage >lua
+---     local body, sdef, vals = create_struct_body(dadta)
+--- <
 local function create_struct_body(data)
     assert(type(data) == "table", "Error: bad type for data")
     local body = {}
@@ -25,22 +31,34 @@ local function create_struct_body(data)
     local sdef = data[1].sdef
     local indent = "        "
     for _, member in ipairs(data) do
-        table.insert(body, string.format(
-            indent .. "%s %s;",
-            member.type,
-            member.name
-        ))
+        table.insert(body, string.format(indent .. "%s %s;", member.type, member.name))
         -- size always reflects __packed__ (no padding)
         size = size + ffi.sizeof(member.type)
         assert(member.sdef == sdef, "Error: unmatched name")
         table.insert(vals, member.init)
     end
-    return string.format(
-        _template,
-        table.concat(body, "\n"), size, sdef
-    ), sdef, vals
+    return string.format(_template, table.concat(body, "\n"), size, sdef), sdef, vals
 end
 
+--- Creates a `cdata` type constructor for creating instances of a C struct.
+--- Function takes a table of data describing a C structure, creates a cdef
+--- string, registers it with FFI registry and attaches a metatable to the
+--- constructor.
+---@param table data Describes all parts of a C structure including init vals
+---@return cdata constructor An FFI `cdata` type constructor
+---@return table vals A table of values for initialization
+---
+---@usage >lua
+---     local struct = require("struct")
+---     local data = require("data")
+---     local MyConstructor, vals = struct.create_new_struct(data)
+---     local instance = MyConstructor(vals)
+---     print(m)
+---     print(m:size())
+---     local file = assert(io.open("table.bin", "wb"))
+---     file:write(m:to_bytes())
+---     file:close()
+--- <
 function TableGen.create_new_struct(data)
     local body, sdef, vals = create_struct_body(data)
     ffi.cdef(body)
@@ -70,7 +88,8 @@ function TableGen.create_new_struct(data)
             end
             return table.concat(out)
         end,
-    }), vals
+    }),
+        vals
 end
 
 return TableGen
